73a315615d10906698a648dc911acc1cc629e55a
채소 <chlthgus0901@naver.com>
2024-07-28 02:29:23 +0900

    dataList생성

---
+++ src/data/dataList.js
@@ -0,0 +1,26 @@
+const dataList = [
+  {
+    name: "[풀무원] 탱탱쫄면 (4개입)",
+    price: 4980,
+    quantity: 3,
+    src: "./assets/product01",
+  },
+  {
+    name: "유기농 밀키퀸 현미 4kg",
+    price: 25000,
+    quantity: 1,
+    src: "./assets/product03",
+  },
+  {
+    name: "[프로쉬] 베이비 세탁세제",
+    price: 18900,
+    quantity: 2,
+    src: "./assets/product04",
+  },
+  {
+    name: "[온더바디] 죠르디 시카 자석 선쿠션",
+    price: 4980,
+    quantity: 1,
+    src: "./assets/product02",
+  },
+];
